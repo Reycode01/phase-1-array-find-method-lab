5f5c887fc3a939df505164b4750482015036cadf
humphrey <writernic89@gmail.com>
2024-03-29 12:59:40 +0300

    add titleCased

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function superbowlWin(record) {
-    const winningGame = record.find(game => game.result === "W");
-    return winningGame ? winningGame.year : undefined;
-  }
-  
-  
-  
+const tutorials = [
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
+];
+
+function capitalizeFirstLetter(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    return tutorial.split(' ').map(capitalizeFirstLetter).join(' ');
+  });
+}
+
+console.log(titleCased());
