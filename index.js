fd78cdeb22fa1eb2502dd8ad02699a427f61a2bf
humphrey <writernic89@gmail.com>
2024-03-29 13:15:04 +0300

    error changes

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const tutorials = [
-  "what does the this keyword mean?",
-  "What is the Constructor OO pattern?",
-  "implementing Blockchain Web API",
-  "The Test Driven Development Workflow",
-  "What is NaN and how Can we Check for it",
-  "What is the difference between stopPropagation and preventDefault?",
-  "Immutable State and Pure Functions",
-  "what is the difference between == and ===?",
-  "what is the difference between event capturing and bubbling?",
-  "what is JSONP?",
-];
-
-function capitalizeFirstLetter(word) {
-  return word[0].toUpperCase() + word.slice(1);
+function superbowlWin(record) {
+  const winningYear = record.find(game => game.result === "W");
+  return winningYear ? winningYear.year : undefined;
 }
 
-function titleCased() {
-  return tutorials.map((tutorial) => {
-    return tutorial.split(' ').map(capitalizeFirstLetter).join(' ');
-  });
-}
+module.exports = { superbowlWin };
 
-console.log(titleCased());
